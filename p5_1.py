51abd3c426e4a9eb3e38ed6a145c3f30ec736d50
Michael Merickel <michael@merickel.org>
2019-12-08 20:56:04 -0600

    fix p5_1 tests

--- p5_1.py
+++ p5_1.py
@@ -81,9 +81,6 @@ def parse_instruction(ctx):
     ))
     return op, params
 
-def join_params(params, modes):
-    return list(zip(params, modes))
-
 def run_program(program, input=()):
     ctx = Runtime(program, iter(input))
     while not ctx.halt:
@@ -92,13 +89,13 @@ def run_program(program, input=()):
         ctx.i += op.num_params + 1
     return ctx.output
 
-@pytest.mark.parametrize('program,expected', [
+@pytest.mark.parametrize('input,expected', [
     ('1,0,0,0,99', '2,0,0,0,99'),
     ('2,3,0,3,99', '2,3,0,6,99'),
     ('2,4,4,5,99,0', '2,4,4,5,99,9801'),
     ('1,1,1,4,99,5,6,0,99', '30,1,1,4,2,5,6,0,99'),
 ])
-def test(program, expected):
+def test(input, expected):
     program = parse_program(input)
     run_program(program)
     result = ','.join(str(x) for x in program)
